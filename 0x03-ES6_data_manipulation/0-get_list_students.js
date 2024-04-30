5a944b06d3a2778c29dc5b81f68fa3a81f03f475
root <you@example.com>
2024-04-30 00:39:37 -0700

    List student

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,7 @@
+/**
+ * Func retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
